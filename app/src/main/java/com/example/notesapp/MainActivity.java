93ffdf09ee785fef372cd829babc2c428aa2c6e6
mseferi <seferimateo1994@gmail.com>
2021-06-02 10:16:26 +0200

    Initial commit Created project, added colors and themes and notes_activity_menu

---
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notes_activity_menu, menu); // inflate toolbar with our menu layout
+        return true;
+    }
+}
